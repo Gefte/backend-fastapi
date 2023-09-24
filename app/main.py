966e50e9252209da80c1a3a6616888e3ecc3e100
Gefte <gefte.95@hotmail.com>
2023-09-24 10:24:44 -0300

    Primeiro commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from typing import List
+
+app = FastAPI()
+
+def fibonacci_recursive(qtd_elementos: int) -> List[int]:
+    if qtd_elementos <= 0:
+        return []
+    elif qtd_elementos == 1:
+        return [0]
+    elif qtd_elementos == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < qtd_elementos:
+            next_element = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_element)
+        return fib_sequence
+
+@app.get("/health", response_model=str)
+def read_root():
+    return {"Estou saudavel"}
+
+
+@app.get("/fibonacci/{qtd_elementos}", response_model=List[int])
+def get_fibonacci_sequence(qtd_elementos: int):
+    if qtd_elementos <= 0:
+        raise HTTPException(status_code=400, detail="A quantidade de elementos deve ser maior que zero")
+    return fibonacci_recursive(qtd_elementos)
